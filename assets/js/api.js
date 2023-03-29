328e90e09d9e837ca9a7e4650e31b13f97c55f18
xyra283 <miko2833@interia.pl>
2023-03-29 20:10:18 +0200

    ook

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,3 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
-
 "use strict";
 
 const api_key = "76313e14d59878157e706303af9fd871";
